da2e6d940a196b86c5058051c53532e16ab9a090
danieluramg <daniel.uramg@gmail.com>
2018-04-27 10:59:36 -0300

    Update bancointerautologin.user.js

--- bancointerautologin.user.js
+++ bancointerautologin.user.js
@@ -1,9 +1,7 @@
 /*
-// @description
-Injeta na página uma popup jQuery para escolher entre conta PF e PJ (MEI) para preencher automaticamente os campos
-e injeta um campo de senha Digitável, que simula os cliques no teclado numérico.
+// @description Injeta na página uma popup jQuery para escolher entre conta PF e PJ (MEI) para preencher automaticamente os campos e injeta um campo de senha Digitável, que simula os cliques no teclado numérico.
 
-Preencha o número de suas contas PF e PJ (MEI) nas linhas 30 e 31 (Entre as ' ')
+Preencha o número de suas contas PF e PJ (MEI) nas linhas 28 e 29 (Entre as ' ')
 Se não tiver uma conta PJ pode deixar em branco o campo correspondente, assim não irá aparecer a popup e será
 preenchido automaticamente com o número de sua conta PF
 
